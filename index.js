df872a4d42958d7849c20e41012c69081ec1f351
noah <neoptunium@gmail.com>
2022-03-20 14:20:33 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { Client, Collection, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_PRESENCES] });
 const { token } = require('./config.json');
 const fs = require('fs');
 
