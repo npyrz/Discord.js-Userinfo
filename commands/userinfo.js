eb41d57316498f57dc62744d45b8ba3a19b3f539
noah <neoptunium@gmail.com>
2022-03-21 17:15:39 -0500

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -4,12 +4,18 @@ const { MessageEmbed } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('userinfo')
-		.setDescription(`See cool & important information about yourself!`),
+		.setDescription(`See info about yourself 💁`),
 	async execute(interaction) {
 		var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric'};
 		let rolemap = interaction.member.roles.cache.sort((a, b) => b.position - a.position).map(r => r).join(" ");
 		if (rolemap.length > 1024) rolemap = "`The user has to many roles to display all of them!`";
 		if (!rolemap) rolemap = "`The user doesn't have any roles!`";
+		let status = {
+			online: 'Online',
+			idle: 'Idle',
+			dnd: 'Do Not Disturb',
+			offline: 'Offline/Invisible'
+		};
 	const exampleEmbed = new MessageEmbed()
 	.setAuthor({ name: `${interaction.user.tag}`, iconURL: `${interaction.user.displayAvatarURL()}` })
 	.setColor(interaction.user.hexAccentColor)
@@ -21,14 +27,14 @@ module.exports = {
 		{ name: '<:ClydeBot:612740739495100419> '+' Bot', value: "`"+`${interaction.user.bot}`+"`", inline: true },
         { name: '<:voltic_join:932381522286227508> '+' Account Created', value: `<t:`+`${Math.floor(interaction.user.createdTimestamp/1000)}`+`:R>`, inline: true },
 		{ name: '<a:voltic_join:875440963697381437> '+' Joined', value: `<t:`+`${Math.floor(interaction.member.joinedTimestamp/1000)}`+`:R>`, inline: true },
-		{ name: '<a:plexiOnline:478870259944783873>'+' Status', value: "`"+`${interaction.member.presence.status}`+"`", inline: true },
+		{ name: '<a:plexiOnline:478870259944783873>'+' Status', value: "`"+`${status[interaction.member.presence.status]}`+"`", inline: true },
 		{ name: '<:booster:660789028861509633>'+' Server Boosting Since', value: "`"+`${interaction.member.premiumSince?.toLocaleDateString("en-US", options) || "Not Boosting"}`+"`", inline: true },
 		{ name: '🔇'+' Last Timeout', value: "`"+`${interaction.member.communicationDisabledUntil?.toLocaleDateString("en-US", options) || "Never Timed Out"}`+"`", inline: true },
 		{ name: '🎧'+' Voice Channel', value: `${interaction.member.voice.channel || "`Currently not in a Voice Channel`"}`, inline: true },
 		{ name: '<:role:923462604293300224>'+' Roles', value: rolemap, inline: true },
 		{ name: '📜'+' Permissions', value: "`"+`${interaction.member.permissions.toArray()}`+"`", inline: false },
 	)
-	.setTimestamp()
+	.setTimestamp();
 	interaction.reply({ embeds: [exampleEmbed]});
 	},
 };
